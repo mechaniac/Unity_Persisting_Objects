694a62a20990a7d486a69790ba87c7e231c537f5
Christof Stanits <christofstanits@gmail.com>
2020-02-29 09:08:35 +0100

    first commit: first chapter

---
+++ PersistingObjects/Assets/Scripts/PersistableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[DisallowMultipleComponent]
+public class PersistableObject : MonoBehaviour
+{
+
+    public virtual void Save(GameDataWriter writer)
+    {
+        writer.Write(transform.localPosition);
+        writer.Write(transform.localRotation);
+        writer.Write(transform.localScale);    
+    }
+
+    public virtual void Load(GameDataReader reader)
+    {
+        transform.localPosition = reader.ReadVector3();
+        transform.localRotation = reader.ReadQuaternion();
+        transform.localScale = reader.ReadVector3();
+    }
+}
